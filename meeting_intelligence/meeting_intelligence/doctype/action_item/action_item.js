d3d310b57b051192f46ee8a9e69ee1552a63b10a
Dhanaalakshmi-23 <dhanaalakshminarayanan@gmail.com>
2026-09-13 20:10:12 +0530

    feat: add MeetMind core DocTypes

---
+++ meeting_intelligence/meeting_intelligence/doctype/action_item/action_item.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Dhanaa Lakshmi and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Action Item", {
+// 	refresh(frm) {
+
+// 	},
+// });
